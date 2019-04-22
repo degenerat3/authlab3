614628ff0a4a7bb05d8156501a0cfbde6e1351f3
JonahChrzanowski <42982132+JonahChrzanowski@users.noreply.github.com>
2019-04-22 12:39:44 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,16 @@
+import os
 import requests
 from cryptography.fernet import Fernet
 
-ip = input("IP Address of Authentication Server: ")
+ip = os.environ.get("auth_serv")
 username = input("Username: ")
 password = input("Password: ")
 key = Fernet.generate_key()
 cipher_suite = Fernet(key)
 cipher_text = cipher_suite.encrypt(password.encode())
 r = requests.post('http://'+ip+':5000/auth', json={"username": username, "password": cipher_text, "key": key})
+token = r.content
+
+app = os.environ.get("app_serv")
+nr = requests.post('http://'+app+':5000/gimme', json={"password": password, "token": token})
+print(nr.status_code)
