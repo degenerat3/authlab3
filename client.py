30de198f2c78ef1ee16588fc874c886ebe752c57
JonahChrzanowski <42982132+JonahChrzanowski@users.noreply.github.com>
2019-04-22 11:32:48 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,10 @@
 import requests
 from cryptography.fernet import Fernet
 
+ip = input("IP Address of Authentication Server: ")
 username = input("Username: ")
 password = input("Password: ")
-#password = password.rjust(16, '0')
 key = Fernet.generate_key()
-#print(type(key))
 cipher_suite = Fernet(key)
 cipher_text = cipher_suite.encrypt(password.encode())
-r = requests.post('http://10.150.101.40:5000/auth', json={"username": username, "password": cipher_text, "key": key})
+r = requests.post('http://'+ip+':5000/auth', json={"username": username, "password": cipher_text, "key": key})
