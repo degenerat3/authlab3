bc9bd069caf6e274699ea436dbe94807b3b891a8
JonahChrzanowski <42982132+JonahChrzanowski@users.noreply.github.com>
2019-04-22 13:50:10 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,17 @@
 import os
 import requests
 from cryptography.fernet import Fernet
+import hashlib
+import base64
+
+def dec_tok(tok, password):
+    hashed = hashlib.sha256(password.encode()).digest()
+    print("LENGTH" + str(len(hashed)))
+    key = base64.urlsafe_b64encode(hashed)
+    cs = Fernet(key)
+    et = cs.decrypt(tok)
+    print("DEC TOK: " + et.decode())
+    return et
 
 ip = os.environ.get("auth_serv")
 username = input("Username: ")
@@ -10,7 +21,8 @@ cipher_suite = Fernet(key)
 cipher_text = cipher_suite.encrypt(password.encode())
 r = requests.post('http://'+ip+':5000/auth', json={"username": username, "password": cipher_text, "key": key})
 token = r.content
+pt_tok = dec_tok(token, password)
 
 app = os.environ.get("app_serv")
-nr = requests.post('http://'+app+':5000/gimme', json={"token": token})
+nr = requests.post('http://'+app+':5000/gimme', json={"token": pt_tok})
 print(nr.status_code)
